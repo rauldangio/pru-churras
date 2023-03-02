45971bdefe0bfafeee4a3bb148e6bd6170b9fdcd
rauldangio <jojofagqwrt@gmail.com>
2023-03-02 12:25:03 -0300

    finalizando caraCoroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  logo = 'assets/logo.png'
+  cara_coroa = ['assets/cara.png', 'assets/coroa.png']
+  image = this.logo;
+  resultado = 'Clique no botao para jogar'
 
   constructor() {}
 
+  jogarMoeda(){
+    if(Math.random() < 0.5){
+      this.image = this.cara_coroa[0]
+      this.resultado = 'Cara!'
+    }else{
+      this.image = this.cara_coroa[1]
+      this.resultado = 'Coroa!'
+    }
+  }
 }
