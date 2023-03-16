d4aba0fe2a04fb507330e41c70151cf7bfb39ec4
rauldangio <jojofagqwrt@gmail.com>
2023-03-16 12:17:41 -0300

    contador de truco finalizado

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,59 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  valendo = 1;
+  time1_score = 0;
+  time2_score = 0;
+  time1_wins = 0;
+  time2_wins = 0;
 
+  constructor() { }
+
+  change_contador(contador: number) {
+    this.valendo = contador
+  }
+
+  increase_team1_score() {
+    this.time1_score += this.valendo
+    this.valendo = 1
+    if (this.time1_score >= 12) {
+      this.time1_wins += 1
+      this.time1_score = 0
+    }
+  }
+
+  decrease_team1_score() {
+    if (this.time1_score <= 0) {
+      // do nothing
+    } else {
+      this.time1_score -= this.valendo
+      this.valendo = 1
+    }
+  }
+
+  increase_team2_score() {
+    this.time2_score += this.valendo
+    this.valendo = 1
+    if (this.time2_score >= 12) {
+      this.time2_wins += 1
+      this.time2_score = 0
+    }
+  }
+
+  decrease_team2_score() {
+    if (this.time2_score <= 0) {
+      // do nothing
+    } else {
+      this.time2_score -= this.valendo
+      this.valendo = 1
+    }
+  }
+
+  clean() {
+    this.valendo = 1;
+    this.time1_score = 0;
+    this.time2_score = 0;
+    this.time1_wins = 0;
+    this.time2_wins = 0;
+  }
 }
